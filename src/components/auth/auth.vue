5ac19a9c5909889bb226c1f7eb0b155dd2a3649f
hongchh <hongchh_sysu@qq.com>
2017-01-22 11:12:40 +0800

    添加依赖使项目支持jade+sass开发

---
+++ src/components/auth/auth.vue
@@ -0,0 +1,15 @@
+<template lang="jade">
+  div#auth
+    h1 Auth Page
+</template>
+
+<script>
+  export default {
+    name: 'auth'
+  }
+</script>
+
+<style lang="sass">
+  #auth
+    border: 1px solid red
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,11 +4,13 @@ import Router from 'vue-router'
 Vue.use(Router)
 
 import Hello from '../components/Hello'
+import Auth from '../components/auth/auth'
 
 export default new Router({
   mode: 'history',
   routes: [
     { path: '/hello', component: Hello },
+    { path: '/auth', component: Auth },
     { path: '/', redirect: '/hello' }
   ]
 })
