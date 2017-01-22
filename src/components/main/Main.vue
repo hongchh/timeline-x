fe88133020070a36961cef7b61a69059dce74f77
hongchh <hongchh_sysu@qq.com>
2017-01-22 20:15:53 +0800

    完善主界面顶栏功能

--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -1,20 +1,34 @@
 <template lang="jade">
 div#main
   div#header
-    el-row(:gutter="20", :justify="center")
-      el-col(:span="2", :offset="16")
-        span.header-item 时光展
+    el-row(:gutter="20")
+      el-col(:span="1", :offset="1")
+        i.el-icon-time#logo
+      el-col(:span="2", :offset="14")
+        el-dropdown
+          span.header-item  时光展 <i class="el-icon-caret-bottom el-icon--right"></i>
+          el-dropdown-menu(slot="dropdown")
+            el-dropdown-item
+              router-link.menu-link(to="timeline") 轴线图
+            el-dropdown-item
+              router-link.menu-link(to="time-slide") 轮播图
       el-col(:span="2")
-        span.header-item 管理
+        router-link.header-item(to="management") 管理
       el-col(:span="2")
-        span.header-item 锁屏
+        span.header-item(@click="lockScreen") 锁屏
   div#body
     router-view
 </template>
 
 <script>
 export default {
-  name: 'main'
+  name: 'main',
+  methods: {
+    lockScreen () {
+      // TODO: 锁屏并跳转回到auth
+      this.$message('锁屏，待实现...')
+    }
+  }
 }
 </script>
 
@@ -28,12 +42,13 @@ export default {
     height: 100px
     position: absolute
     background-color: rgba(21, 70, 251, 0.75)
-    color: white
     line-height: 100px
     .el-row
       vertical-align: middle
-      .header-item
+      .header-item, #logo
         font-size: 25px
+        color: white
+        text-decoration: none
       .header-item:hover
         color: #79f7af
         cursor: pointer
@@ -41,4 +56,9 @@ export default {
     width: 100%
     position: absolute
     top: 100px
+
+.el-dropdown-menu .menu-link
+  font-size: 20px
+  text-decoration: none
+  color: black
 </style>
