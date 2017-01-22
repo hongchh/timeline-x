09e7a442fc32835b588f02e006c806ad14b780b4
hongchh <hongchh_sysu@qq.com>
2017-01-22 16:26:16 +0800

    增加界面过渡动画

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
-<template>
-  <div id="app">
-    <router-view></router-view>
-  </div>
+<template lang="jade">
+  div#app
+    transition(name="fade")
+      router-view
 </template>
 
 <script>
@@ -10,12 +10,16 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0;
-  left: 0;
-}
+<style lang="sass">
+#app
+  width: 100%
+  height: 100%
+  position: absolute
+  top: 0
+  left: 0
+.fade-enter-active, .fade-leave-active
+  transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+.fade-enter, .fade-leave-active
+  transform: scale(1.2)
+  opacity: 0
 </style>
--- src/components/main/Main.vue
+++ src/components/main/Main.vue
@@ -1,12 +1,23 @@
 <template lang="jade">
-  div#main
-    h1 Main
-    div#sub-view
-      router-view
+div#main
+  h1 Main
+  div#sub-view
+    //- transition(name="slide-left")
+    router-view
 </template>
 
 <script>
-  export default {
-    name: 'main'
-  }
+export default {
+  name: 'main'
+}
 </script>
+
+<style lang="sass">
+// .slide-left-enter-active
+//   transition: all .3s ease
+// .slide-left-leave-active
+//   transition: all .8s cubic-bezier(1.0, 0.5, 0.8, 1.0)
+// .slide-left-enter, .slide-left-leave-active
+//   left: -100%
+//   opacity: 0
+</style>
