330fc1f41401e00da147d04b04484312df773e9d
hongchh <hongchh_sysu@qq.com>
2017-01-24 13:59:10 +0800

    实现月份时间分析组件按照选择月份展示对应分析结果的功能

--- src/components/main/management/TimeAnalysisPerMonth.vue
+++ src/components/main/management/TimeAnalysisPerMonth.vue
@@ -4,7 +4,7 @@ div#time-analysis-per-month
     div#bar-chart-month
     el-row(:gutter="20")
       el-col(:span="6", :offset="3")
-        el-date-picker(v-model="month", type="month", placeholder="选择月份")
+        el-date-picker(v-model="month", type="month", placeholder="选择月份", @change="drawChart")
       el-col(:span="5", :offset="1")
         el-input(v-model="totalTime", :disabled="true", placeholder="Total:")
       el-col(:span="5", :offset="1")
@@ -23,6 +23,14 @@ export default {
       avgTime: 'AVG: '
     }
   },
+  mounted () {
+    // 按照昨天来设置默认年月份，因为今天的时间可能还没有记录
+    let yesterday = new Date((new Date()).getTime() - 86400000)
+    let month = yesterday.getMonth() + 1
+    if (month < 10) month = '0' + month
+    this.month = yesterday.getFullYear() + '-' + month
+    this.drawChart()
+  },
   computed: {
     // 获取选中的年月份的记录并计算每天的总时间
     timeRecordsOfMonth () {
@@ -39,79 +47,75 @@ export default {
       })
     }
   },
-  mounted () {
-    // 按照昨天来设置默认年月份，因为今天的时间可能还没有记录
-    let yesterday = new Date((new Date()).getTime() - 86400000)
-    let month = yesterday.getMonth() + 1
-    if (month < 10) month = '0' + month
-    this.month = yesterday.getFullYear() + '-' + month
+  methods: {
+    drawChart (val) {
+      if (val) this.month = val
+      // 准备数据，为了防止数据为空导致渲染不出图表，所以在数据为空的项使用默认值0
+      let totalOfMonth = 0
+      let data = new Array(31).fill(0)
+      let dataAxis = new Array(31).fill(0).map((v, i) => i)
+      for (let record of this.timeRecordsOfMonth) {
+        data[Number.parseInt(record.date)] = record.totalTimeOfDay
+        totalOfMonth += record.totalTimeOfDay
+      }
+      this.totalTime = 'Total: ' + totalOfMonth
+      this.avgTime = 'AVG: ' + (totalOfMonth / this.timeRecordsOfMonth.length)
 
-    // 准备数据，为了防止数据为空导致渲染不出图表，所以在数据为空时使用默认数据
-    let totalOfMonth = 0
-    let data = this.timeRecordsOfMonth === [] ? new Array(31).fill(0) : []
-    let dataAxis = this.timeRecordsOfMonth === [] ? new Array(31).map((v, i) => i + 1) : []
-    for (let record of this.timeRecordsOfMonth) {
-      data.push(record.totalTimeOfDay)
-      dataAxis.push(record.date)
-      totalOfMonth += record.totalTimeOfDay
-    }
-    this.totalTime = 'Total: ' + totalOfMonth
-    this.avgTime = 'AVG: ' + (totalOfMonth / this.timeRecordsOfMonth.length)
-
-    // 渲染柱形图的配置
-    let option = {
-      title: {
-        text: '每月时间统计',
-        subtext: '单位：小时',
-        x: '5%'
-      },
-      tooltip: {
-        trigger: 'item',
-        formatter: '{c} 小时'
-      },
-      xAxis: {
-        data: dataAxis,
-        axisLabel: {
-          inside: true,
-          textStyle: {
-            color: '#fff'
-          }
+      // 渲染柱形图的配置
+      let option = {
+        title: {
+          text: '每月时间统计',
+          subtext: '单位：小时',
+          x: '5%'
         },
-        axisTick: { show: false },
-        axisLine: { show: false },
-        z: 10
-      },
-      yAxis: {
-        max: 24,
-        axisLine: { show: false },
-        axisTick: { show: false },
-        axisLabel: { textStyle: { color: '#999' } }
-      },
-      dataZoom: [{ type: 'inside' }],
-      series: [{
-        type: 'bar',
-        itemStyle: {
-          normal: {
-            color: new Echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              {offset: 0, color: '#83bff6'},
-              {offset: 0.5, color: '#188df0'},
-              {offset: 1, color: '#188df0'}
-            ])
+        tooltip: {
+          trigger: 'item',
+          formatter: '{c} 小时'
+        },
+        xAxis: {
+          data: dataAxis,
+          axisLabel: {
+            inside: true,
+            textStyle: {
+              color: '#fff'
+            }
           },
-          emphasis: {
-            color: new Echarts.graphic.LinearGradient(0, 0, 0, 1, [
-              {offset: 0, color: '#2378f7'},
-              {offset: 0.7, color: '#2378f7'},
-              {offset: 1, color: '#83bff6'}
-            ])
-          }
+          axisTick: { show: false },
+          axisLine: { show: false },
+          z: 10
         },
-        data: data
-      }]
-    }
+        yAxis: {
+          max: 24,
+          axisLine: { show: false },
+          axisTick: { show: false },
+          axisLabel: { textStyle: { color: '#999' } }
+        },
+        dataZoom: [{ type: 'inside' }],
+        series: [{
+          type: 'bar',
+          itemStyle: {
+            normal: {
+              color: new Echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {offset: 0, color: '#83bff6'},
+                {offset: 0.5, color: '#188df0'},
+                {offset: 1, color: '#188df0'}
+              ])
+            },
+            emphasis: {
+              color: new Echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                {offset: 0, color: '#2378f7'},
+                {offset: 0.7, color: '#2378f7'},
+                {offset: 1, color: '#83bff6'}
+              ])
+            }
+          },
+          data: data
+        }]
+      }
 
-    let chart = Echarts.init(document.querySelector('#bar-chart-month'))
-    chart.setOption(option)
+      let chart = Echarts.init(document.querySelector('#bar-chart-month'))
+      chart.setOption(option)
+    }
   }
 }
 </script>
